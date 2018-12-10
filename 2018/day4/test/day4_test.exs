31c77981b32ffe35f16f44c8e83ae900b037ce96
happysalada <raphael.megzari@gmail.com>
2018-12-11 07:58:43 +0900

    add day4

---
+++ 2018/day4/test/day4_test.exs
@@ -0,0 +1,34 @@
+defmodule Day4Test do
+  use ExUnit.Case
+  doctest Day4
+
+  def input() do
+    """
+    [1518-11-01 00:00] Guard #10 begins shift
+    [1518-11-01 00:05] falls asleep
+    [1518-11-01 00:25] wakes up
+    [1518-11-01 00:30] falls asleep
+    [1518-11-01 00:55] wakes up
+    [1518-11-01 23:58] Guard #99 begins shift
+    [1518-11-02 00:40] falls asleep
+    [1518-11-02 00:50] wakes up
+    [1518-11-03 00:05] Guard #10 begins shift
+    [1518-11-03 00:24] falls asleep
+    [1518-11-03 00:29] wakes up
+    [1518-11-04 00:02] Guard #99 begins shift
+    [1518-11-04 00:36] falls asleep
+    [1518-11-04 00:46] wakes up
+    [1518-11-05 00:03] Guard #99 begins shift
+    [1518-11-05 00:45] falls asleep
+    [1518-11-05 00:55] wakes up
+    """
+  end
+
+  test "part 1" do
+    assert Day4.part1(input()) == 240
+  end
+
+  test "part 2" do
+    assert Day4.part2(input()) == 4455
+  end
+end
