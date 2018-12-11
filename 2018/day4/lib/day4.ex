23b3b7db9c2a6baaaae14c287fa19e11d54f6532
happysalada <raphael.megzari@gmail.com>
2018-12-11 09:35:12 +0900

    use nimble parsec for day4

--- 2018/day4/lib/day4.ex
+++ 2018/day4/lib/day4.ex
@@ -3,6 +3,8 @@ defmodule Day4 do
   Documentation for Day4.
   """
 
+  import NimbleParsec
+
   def input() do
     File.read!("./input.txt")
   end
@@ -14,28 +16,8 @@ defmodule Day4 do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&parse_entry/1)
-    |> Enum.sort_by(&elem(&1, 0), &(NaiveDateTime.compare(&1, &2) == :lt))
-    |> Enum.reduce({%{}, nil}, fn
-      {_naive_datetime, guard_number, _}, {asleep_map, _} ->
-        {asleep_map, guard_number}
-
-      {naive_datetime, :asleep}, {asleep_map, guard_number} ->
-        {asleep_map, guard_number, naive_datetime}
-
-      {naive_datetime, :awake}, {asleep_map, guard_number, asleep_start} ->
-        current_asleep =
-          asleep_start.minute..(naive_datetime.minute - 1)
-          |> Enum.map(&{&1, 1})
-          |> Enum.into(%{})
-
-        {Map.update(
-           asleep_map,
-           guard_number,
-           current_asleep,
-           &Map.merge(&1, current_asleep, fn _k, v1, v2 -> v1 + v2 end)
-         ), guard_number}
-    end)
-    |> (&elem(&1, 0)).()
+    |> Enum.sort()
+    |> minutes_asleep()
     |> Enum.map(fn {guard_number, asleep_map} ->
       {guard_number, Map.values(asleep_map) |> Enum.sum(), max_minute(asleep_map)}
     end)
@@ -48,28 +30,8 @@ defmodule Day4 do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&parse_entry/1)
-    |> Enum.sort_by(&elem(&1, 0), &(NaiveDateTime.compare(&1, &2) == :lt))
-    |> Enum.reduce({%{}, nil}, fn
-      {_naive_datetime, guard_number, _}, {asleep_map, _} ->
-        {asleep_map, guard_number}
-
-      {naive_datetime, :asleep}, {asleep_map, guard_number} ->
-        {asleep_map, guard_number, naive_datetime}
-
-      {naive_datetime, :awake}, {asleep_map, guard_number, asleep_start} ->
-        current_asleep =
-          asleep_start.minute..(naive_datetime.minute - 1)
-          |> Enum.map(&{&1, 1})
-          |> Enum.into(%{})
-
-        {Map.update(
-           asleep_map,
-           guard_number,
-           current_asleep,
-           &Map.merge(&1, current_asleep, fn _k, v1, v2 -> v1 + v2 end)
-         ), guard_number}
-    end)
-    |> (&elem(&1, 0)).()
+    |> Enum.sort()
+    |> minutes_asleep()
     |> Enum.map(fn {guard_number, asleep_map} ->
       {minute, times} = asleep_map |> Enum.max_by(&elem(&1, 1))
       {guard_number, minute, times}
@@ -79,41 +41,63 @@ defmodule Day4 do
     |> IO.inspect()
   end
 
+  def minutes_asleep(logs) do
+    {asleep_map, _} =
+      logs
+      |> Enum.reduce({%{}, nil}, fn
+        {_date, _hour, _minute, {:shift, guard_number}}, {asleep_map, _} ->
+          {asleep_map, guard_number}
+
+        {_date, _hour, minute, :sleeps}, {asleep_map, guard_number} ->
+          {asleep_map, guard_number, minute}
+
+        {_date, _hour, end_minute, :wakes_up}, {asleep_map, guard_number, asleep_start} ->
+          current_asleep =
+            asleep_start..(end_minute - 1)
+            |> Enum.map(&{&1, 1})
+            |> Enum.into(%{})
+
+          {Map.update(
+             asleep_map,
+             guard_number,
+             current_asleep,
+             &Map.merge(&1, current_asleep, fn _k, v1, v2 -> v1 + v2 end)
+           ), guard_number}
+      end)
+
+    asleep_map
+  end
+
   def parse_entry(log_entry) do
-    log_entry
-    |> String.split(["[", "] "], trim: true)
-    |> List.to_tuple()
-    |> raw_to_entry()
+    {:ok, [year, month, day, hour, minute, state], _, _, _, _} =
+      log_entry
+      |> parse_log()
+
+    {{year, month, day}, hour, minute, state}
   end
 
-  def raw_to_entry({datetime, "wakes up"}) do
-    {string_to_date(datetime), :awake}
-  end
-
-  def raw_to_entry({datetime, "falls asleep"}) do
-    {string_to_date(datetime), :asleep}
-  end
-
-  def raw_to_entry({datetime, begins_shift}) do
-    [guard_number] = String.split(begins_shift, ["Guard #", " begins shift"], trim: true)
-
-    {string_to_date(datetime), String.to_integer(guard_number), :begins_shift}
-  end
-
-  def string_to_date(
-        <<year::binary-size(4), "-", month::binary-size(2), "-", day::binary-size(2), " ",
-          hour::binary-size(2), ":", minute::binary-size(2)>>
-      ) do
-    NaiveDateTime.new(
-      String.to_integer(year),
-      String.to_integer(month),
-      String.to_integer(day),
-      String.to_integer(hour),
-      String.to_integer(minute),
-      0
-    )
-    |> (&elem(&1, 1)).()
-  end
+  defparsecp(
+    :parse_log,
+    ignore(string("["))
+    |> integer(4)
+    |> ignore(string("-"))
+    |> integer(2)
+    |> ignore(string("-"))
+    |> integer(2)
+    |> ignore(string(" "))
+    |> integer(2)
+    |> ignore(string(":"))
+    |> integer(2)
+    |> ignore(string("] "))
+    |> choice([
+      ignore(string("Guard #"))
+      |> integer(min: 1)
+      |> ignore(string(" begins shift"))
+      |> unwrap_and_tag(:shift),
+      ignore(string("falls asleep")) |> replace(:sleeps),
+      ignore(string("wakes up")) |> replace(:wakes_up)
+    ])
+  )
 
   def max_minute(asleep_map) do
     asleep_map |> Enum.max_by(&elem(&1, 1)) |> (&elem(&1, 0)).()
